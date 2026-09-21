9d38ec3dd45e3a162c66af39b36115f86dc5c7db
zeren27 <zerenulutas@gmail.com>
2026-09-21 15:29:59 -0700

    Initial commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Lab 2 
+// Student name:
+// Student number:
+
+using System;
+using System.Text; //Use only if StringBuilder is needed
+//Do not include any more using statement or any unpermitted type
+
+Console.WriteLine("CPEN223 Lab 2");
+
+//Testing: Write some test cases to test well all methods you are to implement    
+//         This is to demonstrates what test cases you have considered
+//TODO 
+Console.WriteLine($"hex=AAA; Expected value: 2730, Returned value: {Lab2.HexToDecimalNumber("AAA")}");
+
+
+//end Testing code
+
+//Do not change the program skeleton
+public class Lab2
+{
+    // Do not change method headers. 
+    // Only add code where it is requested.
+
+    /// <summary>
+    /// computes decimal equivalent of a hexadecimal number
+    /// </summary>
+    /// <param name="hexadecimal">the hexadecimal number passed as a string</param>
+    /// <returns>decimal equivalent which is a non-negative int</returns>
+    public static int HexToDecimalNumber(string hexadecimal)
+    {
+        //TODO: To Implement
+        return -1;  //to fix 
+    }
+
+    /// <summary>
+    /// checks if str1 and str2 are anagrams
+    /// </summary>
+    /// <param name="str1">the first string</param>
+    /// <param name="str2">the second string</param>
+    /// <returns>true if anagrams, false otherwise</returns>
+    public static bool AreAnagrams(string str1, string str2)
+    {
+        //TODO: Implement
+        return false;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# CPEN223-L2
+
+This is a template repo for Lab2, do not to edit this repository, 
+
+For instructions on how to use this please go to the Canvas page for the lab.
