c85ff3c985f74289e4584c192ba36ae435b15fa2
zeren27 <zerenulutas@gmail.com>
2026-09-22 21:57:12 +0000

    final with comments

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -25,7 +25,7 @@ Console.WriteLine($"CPEN223 Lab 2 for user: {userName}");
 // Test case 1 for AreAnagrams
 // Console.WriteLine($"str1 = listen, str2 = silent, Expected return = True; Returned value: {Lab2.AreAnagrams("listen", "silent")}");
 // Test case 2 for AreAnagrams
-// Console.WriteLine($"str1 = madam curie, str2 =radium came, Expected return = True; Returned value: {Lab2.AreAnagrams("madam curie", "radium came")}");
+// Console.WriteLine($"str1 = '  ', str2 = '  ', Expected return = True; Returned value: {Lab2.AreAnagrams("  ", "  ")}");
 // Test case 3 for AreAnagrams
 // Console.WriteLine($"str1 = ClintEastwood, str2 = OldWestAction, Expected return = False; Returned value: {Lab2.AreAnagrams("ClintEastwood", "OldWestAction")}");
 // Test case 4 for AreAnagrams
@@ -51,19 +51,34 @@ public class Lab2
         int length = hexadecimal.Length;
         int total = 0;
 
+        // Set an index in a for loop to go through each entry in the string until the index reaches the end of the string.
         for (int index = 0; index <= length - 1; index++)
         {
-            int num = 0;
+            // Let entry be the corresponding value of the string at index and let num be the numerical equivalent of that entry.
             char entry = hexadecimal[index];
+            int num = 0;
+            
+            // Check if the entry is between 0 and 9, inclusive. If so, use the conversion formula for numbers.
             if (entry >= '0' && entry <= '9')
             {
                 num = entry - '0';
             }
+
+            // Check if the entry is between A and F, inclusive. If so, use the conversion formula for letters.
             else if (entry >= 'A' && entry <= 'F')
             {
                 num = entry - 'A' + 10;
             }
-            total += num * (int)Math.Pow(16, length-1-index);
+
+            // Set a variable, "value", to keep track of the power of 16 in the method. Multiply the value with 16 until the index reaches length-1-index, since
+            // the power is calculated from right to left.
+            int value = 1;
+            for (int i = 1; i <= length-1-index; i++)
+            {
+                value *= 16;
+            }
+            // Apply the conversion formula with a variable, "total".
+            total += num * value;
         }
         return total;  //to fix 
     }
@@ -81,26 +96,33 @@ public class Lab2
         int length2 = str2.Length;
         bool check = true;
 
+        // Check if the lengths of the two strings are the same. If they are not, meaning they don't have the same number of characters and therefore 
+        // not an anagram, assign false to the "check" variable.
         if (length1 != length2)
         {
             check = false;
         }
         else
         {
+            // Convert the strings into arrays of char. We do this
             char[] str1tochar = str1.ToCharArray();
             char[] str2tochar = str2.ToCharArray();
 
+            // Sort the arrays into two different arrays of char.
             Array.Sort(str1tochar);
             Array.Sort(str2tochar);
 
+            // Go through the arrays one by one with the for loop.
             for (int index1 = 0; index1 <= length1 - 1; index1++)
             {
+                // Check if the entries at index1 in both arrays are the same. If not, assign false to the "check" variable.
                 if (str1tochar[index1] != str2tochar[index1])
                 {
                     check = false;
                 }
             }
         }
+        // If no change happens, the function will return true.
         return check;
     }
 }
