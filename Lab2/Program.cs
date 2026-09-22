8a141e0f1a85ca8e075f17001643c1b9944239e9
zeren27 <zerenulutas@gmail.com>
2026-09-22 20:59:00 +0000

    both Qs w/o comments

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -13,14 +13,24 @@ Console.WriteLine($"CPEN223 Lab 2 for user: {userName}");
 //Testing: Write some test cases to test well all methods you are to implement    
 //         This is to demonstrates what test cases you have considered
 //TODO 
-// Test case 1
+// Test case 1 for HexToDecimalNumber
 // Console.WriteLine($"hex=AAA; Expected value: 2730, Returned value: {Lab2.HexToDecimalNumber("AAA")}");
-// Test case 2
+// Test case 2 for HexToDecimalNumber
 // Console.WriteLine($"hex=1ABF003; Expected value: 28045315, Returned value: {Lab2.HexToDecimalNumber("1ABF003")}");
-// Test case 3
+// Test case 3 for HexToDecimalNumber
 // Console.WriteLine($"hex=4B82F109; Expected value: 1,266,872,585, Returned value: {Lab2.HexToDecimalNumber("4B82F109")}");
-// Test case 4
+// Test case 4 for HexToDecimalNumber
 // Console.WriteLine($"hex=0; Expected value: 0, Returned value: {Lab2.HexToDecimalNumber("0")}");
+
+// Test case 1 for AreAnagrams
+// Console.WriteLine($"str1 = listen, str2 = silent, Expected return = True; Returned value: {Lab2.AreAnagrams("listen", "silent")}");
+// Test case 2 for AreAnagrams
+// Console.WriteLine($"str1 = madam curie, str2 =radium came, Expected return = True; Returned value: {Lab2.AreAnagrams("madam curie", "radium came")}");
+// Test case 3 for AreAnagrams
+// Console.WriteLine($"str1 = ClintEastwood, str2 = OldWestAction, Expected return = False; Returned value: {Lab2.AreAnagrams("ClintEastwood", "OldWestAction")}");
+// Test case 4 for AreAnagrams
+// Console.WriteLine($"str1 = a red bug, str2 = bag rude, Expected return = False; Returned value: {Lab2.AreAnagrams("a red bug", "bag rude")}");
+
 
 //end Testing code
 
@@ -67,7 +77,30 @@ public class Lab2
     public static bool AreAnagrams(string str1, string str2)
     {
         //TODO: Implement
-        return false;
-    }
+        int length1 = str1.Length;
+        int length2 = str2.Length;
+        bool check = true;
 
+        if (length1 != length2)
+        {
+            check = false;
+        }
+        else
+        {
+            char[] str1tochar = str1.ToCharArray();
+            char[] str2tochar = str2.ToCharArray();
+
+            Array.Sort(str1tochar);
+            Array.Sort(str2tochar);
+
+            for (int index1 = 0; index1 <= length1 - 1; index1++)
+            {
+                if (str1tochar[index1] != str2tochar[index1])
+                {
+                    check = false;
+                }
+            }
+        }
+        return check;
+    }
 }
